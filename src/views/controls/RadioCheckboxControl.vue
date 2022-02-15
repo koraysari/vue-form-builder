f06717a56a09d0a970704f931e6efc17b2b81e42
koraysari <koray@ondokuzon.com>
2022-02-15 10:32:57 +0300

    fix

--- src/views/controls/RadioCheckboxControl.vue
+++ src/views/controls/RadioCheckboxControl.vue
@@ -20,8 +20,9 @@
             </label>
 
             <span v-if="control.items.some((item => item.text === 'Diğer'))">
-                <label class="text-left"><input :name="inputName" v-model="diger" type="radio" class="" value="Diğer" @change="digerChange"> Diğer <!----><br></label>
+                <label class="text-left"><input :name="inputName" v-model="diger" :type="control.type" class="" value="Diğer" @change="digerChange"> Diğer <!----><br></label>
                 <input type="text" class="form-control md-field" v-model="digerValue" @change="digerChangeEvent">
+                <div v-if="!digerValue && diger" class="invalid-feedback">Bu alan zorunludur!</div>
             </span>
 
 
@@ -51,8 +52,9 @@
                 </div>
 
                 <div v-if="control.items.some((item => item.text === 'Diğer'))" :class="[styles.COLUMNS.COL6, positionClasses]">
-                    <label><input :name="inputName" v-model="diger" type="checkbox" class="" value="Diğer" @change="digerChange"> Diğer <!----><br></label>
+                    <label><input :name="inputName" v-model="diger" :type="control.type" class="" value="Diğer" @change="digerChange"> Diğer <!----><br></label>
                     <input type="text" class="form-control md-field" v-model="digerValue" @change="digerChangeEvent">
+                    <div v-if="digerValue && true" class="invalid-feedback">Bu alan zorunludur!</div>
                 </div>
 
             </div>
